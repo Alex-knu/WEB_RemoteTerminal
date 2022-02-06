bf03c6c43f3acbf8c634caf6089f094946ff1607
Alex-knu <arozdobudko2@gmail.com>
2022-02-06 21:44:29 +0200

    Test email

--- WorkWithDB.py
+++ WorkWithDB.py
@@ -4,23 +4,25 @@ import configparser
 config = configparser.ConfigParser()
 config.read("Files/settings.ini")
 
-
 mydb = mysql.connector.connect(
-  host=config["DataBase"]["host"],
-  user=config["DataBase"]["user"],
-  password=config["DataBase"]["password"],
-  database=config["DataBase"]["database"]
+    host=config["DataBase"]["host"],
+    user=config["DataBase"]["user"],
+    password=config["DataBase"]["password"],
+    database=config["DataBase"]["database"]
 )
 
 mycursor = mydb.cursor()
+
 
 def GetHistory():
     result = mycursor.execute("SELECT * FROM HistoryToMachine")
     return [list(i) for i in result]
 
+
 def GetUserMachine():
     result = mycursor.execute("SELECT * FROM UserToMachine")
     return [list(i) for i in result]
+
 
 def GetCommandToUser():
     result = mycursor.execute("SELECT * FROM CommandToUser")
