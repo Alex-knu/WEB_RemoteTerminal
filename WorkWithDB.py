40677276401532bfe3ab4c4e930b0aecf2e728f2
alex-knu <arozdobudko2@gmail.com>
2022-04-15 11:52:12 +0300

    Added new controllers

--- WorkWithDB.py
+++ WorkWithDB.py
@@ -1,31 +1,67 @@
 import mysql.connector
 import configparser
+import uuid
 
 config = configparser.ConfigParser()
 config.read("Files/settings.ini")
 
-mydb = mysql.connector.connect(
-    host=config["DataBase"]["host"],
-    user=config["DataBase"]["user"],
-    password=config["DataBase"]["password"],
-    database=config["DataBase"]["database"]
-)
 
-mycursor = mydb.cursor()
+# !!!Откроем когда будет бд
+# mydb = mysql.connector.connect(
+#     host=config["DataBase"]["host"],
+#     user=config["DataBase"]["user"],
+#     password=config["DataBase"]["password"],
+#     database=config["DataBase"]["database"]
+# )
+#
+# mycursor = mydb.cursor()
 
 
-def GetHistory():
-    result = mycursor.execute("SELECT * FROM HistoryToMachine")
-    return [list(i) for i in result]
+def GetHistory(userGUID):
+    # !!! join таблиц UserToMachine и HistoryToMachine по UserToMachine.GUID = HistoryToMachine.MachineGUID + where
+    # UserToMachine.UserGUID = userGUID !!!
+
+    # result = mycursor.execute("SELECT * FROM HistoryToMachine")
+    # return [list(i) for i in result]
+    return 1
 
 
 def GetUserMachine(machinName, user, password):
     # !!!Прилепить входящие данные функции в фильтрацию запроса!!!
 
-    result = mycursor.execute("SELECT * FROM UserToMachine")
-    return [list(i) for i in result]
+    # result = mycursor.execute("SELECT * FROM UserToMachine")
+    # return [list(i) for i in result]
+    return 1
 
 
 def GetCommandToUser():
-    result = mycursor.execute("SELECT * FROM CommandToUser")
-    return [list(i) for i in result]
+    # result = mycursor.execute("SELECT * FROM CommandToUser")
+    # return [list(i) for i in result]
+    return 1
+
+
+def SaveMachine(userGUID, host, port, username, password):
+    newGUID = uuid.uuid4()
+    # ну и сейв в бд. Без ретурна
+
+
+def SaveHistory(machineGUID, command, time):
+    newGUID = uuid.uuid4()
+    # ну и сейв в бд. Без ретурна
+
+
+def SaveUser(login, password, name):
+    newGUID = uuid.uuid4()
+    # ну и сейв в бд. Без ретурна
+
+def UpdateUser(guid, login, password, name):
+    return 1
+
+def UpdateMachine(guid, host, port, password):
+    return 1
+
+def DeleteUser(guid):
+    return 1
+
+def DeleteMachine(guid):
+    return 1
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import WorkWithDB as wwdb
 
 app = Flask(__name__)
 
+
 def pretty_output(response):
     answ = ""
     for elem in response:
@@ -28,17 +29,75 @@ def Connect():
 
     result = rem.execute_remote_command(host, port, username, password, command)
 
-    #Но по нормальному должно юыло быть так
+    # Но по нормальному должно было быть так
     # data = wwdb.GetUserMachine(request_data['machinName'], request_data['user'], request_data['password'])
     # if data is None or len(data) == 0:
     #     return 'Error: Not found user machin'
     #
+    # wwdb.SaveHistory(data.machineGUID, command, datetime.datetime.now())
     # result = rem.execute_remote_command(data.host, data.port, data.username, data.password, data.command)
 
     return pretty_output(result)
 
-#
+
+@app.route('/Ggthistory', methods=['GET'])
+def GetHistory():
+    request_data = request.get_json()
+
+    userGUID = request_data['userGUID']
+
+    result = wwdb.GetHistory(userGUID)
+
+    return str(result)
+
+
+@app.route('/savemachine', methods=['POST'])
+def SaveMachine():
+    request_data = request.get_json()
+
+    userGUID = request_data['userGUID']
+    host = request_data['host']
+    port = request_data['port']
+    username = request_data['username']
+    password = request_data['password']
+
+    wwdb.SaveMachine(userGUID, host, port, username, password)
+
+
+@app.route('/saveuser', methods=['POST'])
+def SaveUser():
+    request_data = request.get_json()
+
+    login = request_data['login']
+    name = request_data['name']
+    password = request_data['password']
+
+    wwdb.SaveUser(login, name, password)
+
+
+@app.route('/updateuser', methods=['POST'])
+def UpdateUser():
+    request_data = request.get_json()
+
+    guid = request_data['guid']
+    login = request_data['login']
+    name = request_data['name']
+    password = request_data['password']
+
+    wwdb.UpdateUser(guid, login, name, password)
+
+
+@app.route('/updatevachine', methods=['POST'])
+def UpdateMachine():
+    request_data = request.get_json()
+
+    guid = request_data['guid']
+    host = request_data['host']
+    port = request_data['port']
+    password = request_data['password']
+
+    wwdb.UpdateMachine(guid, host, port, password)
+
+
 if __name__ == '__main__':
     app.run()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
