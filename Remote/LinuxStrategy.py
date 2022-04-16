bfabda4cbee381aed91202e7f72790aaeaec2c97
Vladyslav Skyruta <vlad.skyruta@gmail.com>
2022-04-16 17:31:12 +0000

    Bug fix

--- Remote/LinuxStrategy.py
+++ Remote/LinuxStrategy.py
@@ -4,7 +4,13 @@ def execute_linux_local(command):
     return subprocess.run(command, shell=True)
 
 def keygen_linux(hostname):
-    return execute_windows_local(f"ssh-keygen -b 2048 -t rsa -f keys/{hostname}/id_rsa -q -N \"\"")
+    return execute_linux_local(f"ssh-keygen -b 2048 -t rsa -f keys/{hostname}/id_rsa -q -N \"\"")
+
+def check_if_dir_exist(hostname):
+    if execute_linux_local(f'[ -d "keys/{hostname}" ] && echo "true"').returncode == 0:
+        return True
+    else:
+        return False
 
 def create_dir_linux(hostname):
     return execute_linux_local(f'mkdir -p keys/{hostname}')
