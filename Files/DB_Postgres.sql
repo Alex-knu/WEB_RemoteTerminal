2b97451664de504e48cda493cedfddfdfc14a76b
alex-knu <arozdobudko2@gmail.com>
2022-04-17 20:20:14 +0300

    Rename some column in PostgreSQL script

--- Files/DB_Postgres.sql
+++ Files/DB_Postgres.sql
@@ -1,14 +1,14 @@
 CREATE DATABASE RemoteTerminal1;
 
 CREATE TABLE "HistoryToMachine" (
-    guid uuid NOT NULL,
+    "Guid" uuid NOT NULL,
     "MachineGUID" uuid NOT NULL,
     "Command" character varying NOT NULL,
     "Time" date NOT NULL
 );
 
 CREATE TABLE "UserToMachine" (
-    guid uuid NOT NULL,
+    "Guid" uuid NOT NULL,
     "UserGUID" uuid NOT NULL,
     "MachineName" character varying NOT NULL,
     "Host" character varying NOT NULL,
@@ -25,24 +25,24 @@ CREATE TABLE "Users" (
 );
 
 ALTER TABLE ONLY "HistoryToMachine"
-    ADD CONSTRAINT historytomachine_pk PRIMARY KEY (guid);
+    ADD CONSTRAINT historytomachine_pk PRIMARY KEY ("Guid");
 
 ALTER TABLE ONLY "Users"
     ADD CONSTRAINT users_pk PRIMARY KEY ("Guid");
 
 ALTER TABLE ONLY "UserToMachine"
-    ADD CONSTRAINT usertomachine_pk PRIMARY KEY (guid);
+    ADD CONSTRAINT usertomachine_pk PRIMARY KEY ("Guid");
 
-CREATE UNIQUE INDEX historytomachine_guid_uindex ON "HistoryToMachine" USING btree (guid);
+CREATE UNIQUE INDEX historytomachine_guid_uindex ON "HistoryToMachine" USING btree ("Guid");
 
 CREATE UNIQUE INDEX users_guid_uindex ON "Users" USING btree ("Guid");
 
 CREATE UNIQUE INDEX users_login_uindex ON "Users" USING btree ("Login");
 
-CREATE UNIQUE INDEX usertomachine_guid_uindex ON "UserToMachine" USING btree (guid);
+CREATE UNIQUE INDEX usertomachine_guid_uindex ON "UserToMachine" USING btree ("Guid");
 
 ALTER TABLE ONLY "HistoryToMachine"
-    ADD CONSTRAINT historytomachine_usertomachine_guid_fk FOREIGN KEY ("MachineGUID") REFERENCES "UserToMachine"(guid) ON UPDATE CASCADE ON DELETE CASCADE;
+    ADD CONSTRAINT historytomachine_usertomachine_guid_fk FOREIGN KEY ("MachineGUID") REFERENCES "UserToMachine"("Guid") ON UPDATE CASCADE ON DELETE CASCADE;
 
 ALTER TABLE ONLY "UserToMachine"
     ADD CONSTRAINT usertomachine_users_guid_fk FOREIGN KEY ("UserGUID") REFERENCES "Users"("Guid") ON UPDATE CASCADE ON DELETE CASCADE;
