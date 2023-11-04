168677aa79b0f22538d1504199d86e0f15fdbf5f
Vladyslav Skyruta <vlad.skyruta@gmail.com>
2023-11-04 20:28:05 +0200

    Integrate GatewayAPI and WebAPP

--- GatewayAPI/Core/Models/DTO/RunCommandDTO.cs
+++ GatewayAPI/Core/Models/DTO/RunCommandDTO.cs
@@ -6,8 +6,8 @@ namespace GatewayAPI.Core.Models.DTO
         public int Port { get; set; }
         public string Password { get; set; }
         public string Username { get; set; }
-        public bool UseSSHKey { get; set; }
-        public string RootPassword { get; set; }
+        public bool? UseSSHKey { get; set; }
+        public string? RootPassword { get; set; }
         public string Command { get; set; }
     }
 }
