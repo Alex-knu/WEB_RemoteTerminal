9f35c92ad3e49e12300f23d95cd2caf17fe5f3de
Alex Rozdobudko <80115540+Alex-knu@users.noreply.github.com>
2023-11-04 14:37:57 +0200

    add logic for remouteServer

--- GatewayAPI/Program.cs
+++ GatewayAPI/Program.cs
@@ -1,9 +1,27 @@
+using System.Reflection;
+using GatewayAPI.Core.Interfaces;
+using GatewayAPI.Core.Services;
 using GatewayAPI.Extentions.Extentions;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration
+    .AddEnvironmentVariables()
+    .AddUserSecrets(Assembly.GetExecutingAssembly(), true);
 // Add services to the container.
+
+var configuration = builder.Configuration.GetSection("GatewayServer");
+
+builder.Services
+    .AddHttpClient("RemoteServer", client =>
+    {
+        client.BaseAddress = new Uri(configuration.GetValue<string>("RemoteAPIRoute"));
+    });
+
+// Add services to the container.
+
 builder.Services.AddScoped(typeof(IRouteService), typeof(RouteService));
+builder.Services.AddScoped(typeof(IRemoteService), typeof(RemoteService));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
